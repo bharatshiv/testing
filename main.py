e4ec64911685d76f6fe04fda8a323f8f1fbedf09
bharatshiv <76781997+bharatshiv@users.noreply.github.com>
2021-03-29 23:20:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,13 @@
 from flask import *
-from flaskext.mysql import MySQL
+
 
 app=Flask(__name__)
-app.secret_key='hello123'
 
-app.config['MYSQL_DATABASE_HOST']='localhost'
-app.config['MYSQL_DATABASE_USER']='root'
-app.config['MYSQL_DATABASE_PASSWORD']=''
-app.config['MYSQL_DATABASE_DB']='mydata'
-
-mysql=MySQL()
-mysql.init_app(app)
 
 @app.route('/')
 @app.route('/index')
 def home():
-    return render_template('login.html')
+    return("Hello world")
 
-@app.route('/dologin',methods=['POST'])
-def dologin():
-    username=request.form['username']
-    password=request.form['password']
-    mycon=mysql.connect()
-    mycur=mycon.cursor()
-    info=[username,password]
-    mycur.execute("select * from user where username=%s and password=%s",info)
-    data=mycur.fetchone()
-    if(data):
-        session['user']=data[1]
-        return redirect('/dashboard')
-    else:
-        return redirect('/')
 
-@app.route('/dashboard')
-def dashboard():
-    if 'user' in session:
-        return render_template('dashboard.html')
-    else:
-        return redirect('/')
-
-@app.route('/logout')
-def logout():
-    session.pop('user',None)
-    return redirect('/')
 app.run(debug=True)
