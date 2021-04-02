e10dd213f273987351f13fafe139308ad93d39d3
bharatshiv <76781997+bharatshiv@users.noreply.github.com>
2021-04-02 22:49:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,45 @@
-from flask import Flask
+from flask import *
+from flaskext.mysql import MySQL
 
+app=Flask(__name__)
+app.secret_key='hello123'
 
-ob=Flask(__name__)
+app.config['MYSQL_DATABASE_HOST']='localhost'
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']=''
+app.config['MYSQL_DATABASE_DB']='mydata'
 
+mysql=MySQL()
+mysql.init_app(app)
 
-@ob.route('/')
-@ob.route('/index')
+@app.route('/')
 def home():
-    return("Hello world")
+    return render_template('login.html')
 
+@app.route('/dologin',methods=['POST'])
+def dologin():
+    username=request.form['username']
+    password=request.form['password']
+    mycon=mysql.connect()
+    mycur=mycon.cursor()
+    info=[username,password]
+    mycur.execute("select * from user where username=%s and password=%s",info)
+    data=mycur.fetchone()
+    if(data):
+        session['user']=data[1]
+        return redirect('/dashboard')
+    else:
+        return redirect('/')
 
-ob.run(debug=True)
+@app.route('/dashboard')
+def dashboard():
+    if 'user' in session:
+        return render_template('dashboard.html')
+    else:
+        return redirect('/')
+
+@app.route('/logout')
+def logout():
+    session.pop('user',None)
+    return redirect('/')
+app.run(debug=True)
